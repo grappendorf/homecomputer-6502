8fe0c68ca98381995b6d2d1fc45b50feacb3fe77
Dirk Grappendorf <dirk@grappendorf.net>
2015-01-09 02:33:31 +0100

    Clear the input buffer with Ctrl-U

--- firmware/readline.c
+++ firmware/readline.c
@@ -6,10 +6,13 @@
 #define MAX_CHARS 79
 char readline_buffer[MAX_CHARS + 1];
 char *buffer_pos;
-unsigned char last_key = KEY_NONE;
-char last_char = 0;
+
+void delete_character();
 
 char * readline(unsigned char interruptible) {
+  unsigned char last_key = KEY_NONE;
+  char last_char;
+  unsigned char last_modifiers;
   reset_interrupted();
   buffer_pos = readline_buffer;
 
@@ -26,24 +29,20 @@ char * readline(unsigned char interruptible) {
       if (last_key == KEY_NONE) {
         last_key = keys_get_code();
         last_char = keys_getc();
+        last_modifiers = keys_get_modifiers();
 
         if ((last_key) == KEY_BACKSPACE) {
           if (buffer_pos != readline_buffer) {
-            --buffer_pos;
-            if (lcd_get_x() == 0) {
-              lcd_goto(39, lcd_get_y() - 1);
-              lcd_putc(' ');
-              lcd_goto(39, lcd_get_y() - 1);
-            } else {
-              lcd_goto(lcd_get_x() - 1, lcd_get_y());
-              lcd_putc(' ');
-              lcd_goto(lcd_get_x() - 1, lcd_get_y());
-            }
+            delete_character();
           }
         } else if (last_char == '\n') {
           lcd_putc(last_char);
           *buffer_pos = '\0';
           break;
+        } else if (last_modifiers ==  MODIFIER_CTRL && last_key == KEY_U) {
+           while (buffer_pos != readline_buffer) {
+              delete_character();
+            }
         } else if (last_char != 0 && buffer_pos - readline_buffer < MAX_CHARS) {
           lcd_putc(last_char);
           *buffer_pos = last_char;
@@ -56,4 +55,17 @@ char * readline(unsigned char interruptible) {
   }
 
   return readline_buffer;
+}
+
+void delete_character() {
+  --buffer_pos;
+  if (lcd_get_x() == 0) {
+    lcd_goto(39, lcd_get_y() - 1);
+    lcd_putc(' ');
+    lcd_goto(39, lcd_get_y() - 1);
+  } else {
+    lcd_goto(lcd_get_x() - 1, lcd_get_y());
+    lcd_putc(' ');
+    lcd_goto(lcd_get_x() - 1, lcd_get_y());
+  }
 }
